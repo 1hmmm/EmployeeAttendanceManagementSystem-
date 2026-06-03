b33fe514a5a94309935f3badbdf631013ca39f16
1hmmm <139934848+1hmmm@users.noreply.github.com>
2026-06-03 17:37:55 +0800

    Add files via upload

---
+++ question15.py
@@ -0,0 +1,6 @@
+word = input("Masukkan sebuah kata: ")
+
+print(f"Original word: {word}")
+print(f"First character: {word[0]}")
+print(f"Last character: {word[-1]}")
+print(f"Total number of characters: {len(word)}")
